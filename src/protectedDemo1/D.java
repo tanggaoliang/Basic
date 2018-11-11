c9e78e57e8d8e42aab612e712ab5ded14a5bea67
tanggaoliang <1440535574@qq.com>
2018-11-11 10:40:13 +0800

    test                                                                                 1

---
+++ src/protectedDemo1/D.java
@@ -0,0 +1,9 @@
+package protectedDemo1;
+
+public class D extends B {
+        public static void main(String[] args)  {
+            B b=new B();
+            b.b();
+            b.a();
+        }
+}
