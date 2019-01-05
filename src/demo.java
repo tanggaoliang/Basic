1dde27306a5dba013f622f3a17dc8f5837b259ba
Administrator <1440535574@qq.com>
2019-01-05 18:37:55 +0800

    test                                                                                 1

--- src/demo.java
+++ src/demo.java
@@ -4,21 +4,9 @@ import java.util.List;
 
 
 public class demo {
-    public static void main(String[] args) {
-        Integer s = new Integer(9);
-        Integer t = new Integer(9);
-        int a=9;
-        Long u = new Long(9);
-        System.out.println(s==a);
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-    }
+    private static boolean a;
 
-    public static void operator(StringBuffer x, StringBuffer y) {
-        x.append(y);
-        y.setLength(0);
-        y.append(x);
+    public static void main(String[] args) {
+        System.out.println(a);
     }
 }
